ea0ac49e48804d9c96cb4f74685962e2c14cee5e
Lee Elenbaas <lee@elenbaas.org.il>
2013-11-24 00:48:38 +0200

    create the plugin with AMD support
    feature/#1-amd-support

--- src/jquery-knockout-plugin.js
+++ src/jquery-knockout-plugin.js
@@ -1,9 +1,19 @@
-!function($){
-	"use strict";
+"use strict";
 
+(function (factory) {
+if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery', 'knockout'], factory);
+} else {
+    // Browser globals
+    factory(jQuery, ko);
+}
+}(function ($, ko) {
 	$.fn.koApplyBindings = function(viewModel) {
 		return this.each(function() {
 			ko.applyBindings(viewModel, this);
 		});
 	};
-}(jQuery);
+
+	return $;
+}));
