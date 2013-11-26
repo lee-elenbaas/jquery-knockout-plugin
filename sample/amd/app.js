de081f76e93946db641149239e6de02216d63972
Lee Elenbaas <lee@elenbaas.org.il>
2013-11-26 20:17:02 +0200

    updating samples to have local lib fallback
    feature/#1-amd-support

--- sample/amd/app.js
+++ sample/amd/app.js
@@ -1,4 +1,4 @@
-define(['jquery', 'RequireJSSampleModel', 'jquery-knockout-plugin'], function($, Model) {
+define(['jquery', 'model', 'jquery-knockout-plugin'], function($, Model) {
 	$('.model1').koApplyBindings(new Model('Lee'));
 	$('.model2').koApplyBindings(new Model('Elenbaas')).css({ background: 'Green' });
 });
--- sample/amd/requirejs-config.js
+++ sample/amd/requirejs-config.js
@@ -2,10 +2,16 @@
 
 requirejs.config({
 	paths: {
-		jquery: "http://code.jquery.com/jquery-2.0.3.min",
-		"jquery-knockout-plugin": "../src/jquery-knockout-plugin", 
-		knockout: "http://knockoutjs.com/downloads/knockout-3.0.0"
+		jquery: [
+			"http://code.jquery.com/jquery-2.0.3.min",
+			"../lib/jquery-2.0.3.min.js"
+		],
+		knockout: [
+			"http://knockoutjs.com/downloads/knockout-3.0.0",
+			"../lib/knockout-3.0.0.js"
+		],
+		"jquery-knockout-plugin": "../../src/jquery-knockout-plugin"
 	}
 });
 
-requirejs(["RequireJSSampleApp"]);
+requirejs(["app"]);
